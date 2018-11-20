8896743b9d9fa4278ff91bb1f4ba9433ba91429e
evwhiz <evwhiz@gmail.com>
2018-11-19 23:07:24 -0500

    relative filepath and prints

--- quick_start.py
+++ quick_start.py
@@ -1,19 +1,20 @@
 from bs4 import BeautifulSoup
 
 # Quick Start
-with open("/Users/evansanford/Playground/beautiful_soup_bs/the_dormouses_story.html") as fp:
+with open("./the_dormouses_story.html") as fp:
     soup = BeautifulSoup(fp, 'lxml')
 print(soup.prettify())
 
-# Simple ways to navigate the soup data structure
-print("TITLE\n", soup.title)
-print("TITLE NAME\n", soup.title.name)
-print("TITLE PARENT NAME\n", soup.title.parent.name)
-print("P ELEM\n", soup.p)
-print('P ELEM CLASS VALUE\n', soup.p['class'])
-print('A ELEM\n', soup.a)
-print("ALL A ELEM's\n", soup.find_all('a'))
-print("id = 'link3'\n", soup.find(id='link3'))
+print("Simple ways to navigate the soup data structure")
+
+print("soup.title\n", soup.title)
+print("soup.title.name\n", soup.title.name)
+print("soup.title.parent\n", soup.title.parent.name)
+print("soup.p\n", soup.p)
+print("soup.p['class']\n", soup.p['class'])
+print('soup.a\n', soup.a)
+print("soup.find_all('a')\n", soup.find_all('a'))
+print("soup.find(id='link3')\n", soup.find(id='link3'))
 
 # Extracting all URL's found within <a> tags:
 for link in soup.find_all('a'):
